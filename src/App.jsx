b7a9d57822c5f50e0f4fba517a78ff745dc15df9
Andrea Cavallo <astarore@gmail.com>
2023-09-05 16:00:46 +0200

    finished exercise

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
+import AllTheBooks from "./components/AllTheBooks";
+import MyFooter from "./components/MyFooter";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNav />
+      <Welcome />
+      <AllTheBooks />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
